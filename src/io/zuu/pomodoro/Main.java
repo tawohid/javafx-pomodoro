2a26b4a3d879e2208019970b1e3f9d6f5fcc0451
Khale H <zuu@zuu.io>
2017-07-25 16:55:03 +0800

    Created FXML + CSS

---
+++ src/io/zuu/pomodoro/Main.java
@@ -0,0 +1,23 @@
+package io.zuu.pomodoro;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home.fxml"));
+        primaryStage.setScene(new Scene(root, 625, 475));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
